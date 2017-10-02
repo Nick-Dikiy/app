9ab724e18d367cbe8b73492f49d8568a9d914868
Nick Dikiy <nickdikiy.nd@gmail.com>
2017-10-02 22:45:52 +0300

    with stylesheet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+
+import './main.css';
+import './main.scss';
+
 let rightNow = Date('MMMM Do YYYY, h:mm:ss a');
 
 let a=5;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 'use strict';
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
 
 module.exports = {
     context: __dirname + '/src',
@@ -19,7 +22,8 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             NODE_ENV: JSON.stringify(NODE_ENV)
-        })
+        }),
+
     ],
 
     module:{
@@ -33,7 +37,26 @@ module.exports = {
                         presets: ['env']
                     }
                 }
+            },
+            {
+                 test: /\.css$/,
+                 use: [
+                     'style-loader',
+                     'css-loader'
+                 ]
+            },
+
+            {
+                test: /\.(scss|sass)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
             }
+
         ]
     }
 };
